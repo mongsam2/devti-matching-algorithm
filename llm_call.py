b436f0d1f5b97bdbd88af3d83f4f2f2de157fd66
mongsam2 <songjaehyun121476@gmail.com>
2025-12-05 22:27:11 +0900

    fix: 개비티아이 질문 수정

--- llm_call.py
+++ llm_call.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, TypeAdapter
+from pydantic import BaseModel
 from google import genai
 from openai import OpenAI
 import os
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 from typing import List
 
 load_dotenv()
-
 
 class TeamVibe(BaseModel):
     vibe: str
@@ -39,7 +38,7 @@ class TeamList(BaseModel):
     teams: List[Team]
 
 
-def call_llm(type, team_info_list):
+def call_llm(team_info_list):
     """
     Arge:
         - type = "gemini" || "gpt"
@@ -47,33 +46,16 @@ def call_llm(type, team_info_list):
     with open("prompt/explain.txt", "r", encoding="utf-8") as f:
         prompt = f.read()
 
-    if type == "gemini":
-        client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-        response = client.models.generate_content(
-            model="gemini-3-pro-preview",
-            contents=f"{prompt} {team_info_list}",
-            config={
-                "response_mime_type": "application/json",
-                "response_json_schema": TeamList.model_json_schema(),
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    response = client.beta.chat.completions.parse(
+        model="gpt-5.1",
+        messages=[
+            {
+                "role": "system",
+                "content": prompt,
             },
-        )
-
-        response = TeamList.model_validate_json(response.text)
-        print(response)
-        client.close()
-        return response["teams"]
-
-    elif type == "gpt":
-        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-        response = client.beta.chat.completions.parse(
-            model="gpt-5.1",
-            messages=[
-                {
-                    "role": "system",
-                    "content": prompt,
-                },
-                {"role": "user", "content": f"{team_info_list}"},
-            ],
-            response_format=TeamList,
-        )
-        return response.choices[0].message.parsed.teams
+            {"role": "user", "content": f"{team_info_list}"},
+        ],
+        response_format=TeamList,
+    )
+    return response.choices[0].message.parsed.teams
